8d04d3bd7f222a4c4a80ee7c8f215e2394fcd5ec
publicminsu <publicminsu@naver.com>
2023-06-13 00:32:50 +0900

    PC user movement

--- Assets/2.Script/Player/PlayerController.cs
+++ Assets/2.Script/Player/PlayerController.cs
@@ -4,9 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Vector2 MoveDirection { get; private set; }
+    [SerializeField] float _moveSpeed;
+    Rigidbody2D _rigidbody2D;
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + MoveDirection * _moveSpeed);
+    }
     void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
-        MoveDirection=new Vector2(input.x,input.y);
+        MoveDirection = new Vector2(input.x, input.y);
     }
 }
