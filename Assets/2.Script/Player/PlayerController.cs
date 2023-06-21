1dfa1364d19677215353c0d9cdc947e108ad9e1c
publicminsu <publicminsu@naver.com>
2023-06-21 22:58:46 +0900

    Change Player Input Behavior, Make PC version Click to Move

--- Assets/2.Script/Player/PlayerController.cs
+++ Assets/2.Script/Player/PlayerController.cs
@@ -1,11 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 MoveDirection { get; private set; }
+    private Vector2 targetPosition;
     [SerializeField] private float moveSpeed;
     private Rigidbody2D rigid2D;
+    private bool isPressed = false;
     private void Awake()
     {
         rigid2D = GetComponent<Rigidbody2D>();
@@ -16,11 +19,46 @@ public class PlayerController : MonoBehaviour
     }
     private void PlayerMove()
     {
+        if (!isPressed )
+        {
+            return;
+        }
+        else if (Vector2.Distance(targetPosition, transform.position) < 0.1)
+        {
+            return;
+        }
         rigid2D.MovePosition(rigid2D.position + moveSpeed * Time.fixedDeltaTime * MoveDirection);
     }
-    private void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext callbackContext)
     {
-        Vector2 input = value.Get<Vector2>();
+        if (callbackContext.performed || callbackContext.started)
+        {
+            targetPosition = Vector2.positiveInfinity;
+            isPressed = true;
+        }
+        if (callbackContext.canceled)
+        {
+            isPressed = false;
+        }
+        Vector2 input = callbackContext.ReadValue<Vector2>();
         MoveDirection = new Vector2(input.x, input.y);
+    }
+    public void OnPosition(InputAction.CallbackContext callbackContext)
+    {
+        Vector2 input = callbackContext.ReadValue<Vector2>();
+        targetPosition = Camera.main.ScreenToWorldPoint(input);
+        Vector2 targetDirection = (targetPosition - (Vector2)transform.position).normalized;
+        MoveDirection = targetDirection;
+    }
+    public void OnPress(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed || callbackContext.started)
+        {
+            isPressed = true;
+        }
+        if (callbackContext.canceled)
+        {
+            isPressed = false;
+        }
     }
 }
