32f1a682583dc18146d9b8c101436dcdc1a6c381
publicminsu <publicminsu@naver.com>
2023-06-24 15:42:42 +0900

    code refactoring

--- Assets/2.Script/Player/PlayerController.cs
+++ Assets/2.Script/Player/PlayerController.cs
@@ -4,29 +4,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 MoveDirection { get; private set; }
-    private Vector2 targetPosition;
-    [SerializeField] private float moveSpeed;
     private Rigidbody2D rigid2D;
+    private Camera mainCamera;
+    public Vector2 MoveDirection { get; private set; }
+
+    [SerializeField] private float moveSpeed;
+    private Vector2 targetPosition;
     private bool isPressed = false;
     private void Awake()
     {
         rigid2D = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
     }
     private void FixedUpdate()
     {
-        PlayerMove();
+        MoveCharacter();
     }
-    private void PlayerMove()
+    private void MoveCharacter()
     {
-        if (!isPressed )
-        {
-            return;
-        }
-        else if (Vector2.Distance(targetPosition, transform.position) < 0.1)
-        {
-            return;
-        }
+        if (!isPressed){ return; }
+        else if (Vector2.SqrMagnitude(targetPosition - (Vector2)transform.position) < 0.1) { return; }
+
         rigid2D.MovePosition(rigid2D.position + moveSpeed * Time.fixedDeltaTime * MoveDirection);
     }
     public void OnMove(InputAction.CallbackContext callbackContext)
@@ -46,7 +44,7 @@ public class PlayerController : MonoBehaviour
     public void OnPosition(InputAction.CallbackContext callbackContext)
     {
         Vector2 input = callbackContext.ReadValue<Vector2>();
-        targetPosition = Camera.main.ScreenToWorldPoint(input);
+        targetPosition = mainCamera.ScreenToWorldPoint(input);
         Vector2 targetDirection = (targetPosition - (Vector2)transform.position).normalized;
         MoveDirection = targetDirection;
     }
