31b5d0bd040f712584ae25d4bdf564a21b67ea36
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-06-26 15:11:43 +0900

    Add UI base On/Off function

---
+++ Assets/2.Script/Manager/UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EUIBase { NONE = -1, TITLE, INGAME, SETTING, TEXTCHATTING, RULESETTING, END  }
+
+public class UIManager : SingletonMonobehaviour<UIManager>
+{
+    #region Variables
+
+    [SerializeField] private UIBase[] uiBaseList = new UIBase[0];
+
+    #endregion Variables
+
+    #region Unity Events
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        uiBaseList = transform.GetComponentsInChildren<UIBase>();
+    }
+
+    #endregion Unity Events
+
+    #region Methods
+
+    public UIBase GetUIBase(EUIBase idxUIBase) { return this.uiBaseList[(int)idxUIBase]; }
+
+    public void TurnOnUIBase(EUIBase idxUIBase, bool isTurnOffOther = true)
+    {
+        this.uiBaseList[(int)idxUIBase].gameObject.SetActive(true);
+
+        if (!isTurnOffOther) { return; }
+
+        for (EUIBase idx = EUIBase.TITLE; idx < EUIBase.END; idx++)
+        {
+            if (idx == idxUIBase) { continue; }
+
+            uiBaseList[(int)idxUIBase].gameObject.SetActive(false);
+        }
+    }
+
+    public void TurnOffUIBase(bool isAll = true, EUIBase idxUIBase = EUIBase.NONE)
+    {
+        if (isAll)
+        {
+            foreach (UIBase uiBase in uiBaseList) { uiBase.gameObject.SetActive(false); }
+
+            return;
+        }
+
+        this.uiBaseList[(int)idxUIBase].gameObject.SetActive(false);
+    }
+
+    #endregion Methods
+}
