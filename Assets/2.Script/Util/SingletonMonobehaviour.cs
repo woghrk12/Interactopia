d7c07dd731004a360ca3229f56f649d1d2777afa
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-06-26 14:29:30 +0900

    Add Monobehaviour with a singleton pattern

---
+++ Assets/2.Script/Util/SingletonMonobehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SingletonMonobehaviour<T> : MonoBehaviour where T : Component
+{
+    private static T instance = null;
+
+    public static T Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+                if (instance == null) { instance = new GameObject(typeof(T).Name, typeof(T)).GetComponent<T>(); }
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        T[] t_objs = FindObjectsOfType<T>();
+
+        if (t_objs.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (transform.parent != null && transform.root != null) { DontDestroyOnLoad(transform.root.gameObject); }
+        else { DontDestroyOnLoad(gameObject); }
+    }
+}
