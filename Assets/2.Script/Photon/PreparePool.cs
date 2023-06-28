41243a1b2e0bfa3ad9f593599d53eba85593029e
publicminsu <publicminsu@naver.com>
2023-06-28 17:09:27 +0900

    PreparePool for PrefabPool

---
+++ Assets/2.Script/Photon/PreparePool.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PreparePool : MonoBehaviour
+{
+    public List<GameObject> Prefabs;
+    private void Awake()
+    {
+        DefaultPool pool = PhotonNetwork.PrefabPool as DefaultPool;
+        if (pool != null && this.Prefabs!=null) 
+        {
+            foreach(GameObject prefab in this.Prefabs)
+            {
+                pool.ResourceCache.Add(prefab.name, prefab);
+            }
+        }
+    }
+}
