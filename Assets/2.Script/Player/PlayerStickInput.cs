924c7424be1b32be6dfe002485c67d274efd8ab4
publicminsu <publicminsu@naver.com>
2023-07-13 23:02:05 +0900

    Fix UI Stick (Transform to RectTransform)

--- Assets/2.Script/Player/PlayerStickInput.cs
+++ Assets/2.Script/Player/PlayerStickInput.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerStickInput : MonoBehaviour, IMoveDirection, IEndDragHandler, IDragHandler
 {
     [SerializeField][Min(0)] private float movementRange = 50;
 
+    RectTransform rectTransform;
+
+    [SerializeField]
     Vector2 startPosition;
+
+    [SerializeField] Text text;
 
     public Vector2 MoveDirection
     {
         get
         {
-            return ((Vector2)transform.position - startPosition).normalized;
+            return ((Vector2)rectTransform.anchoredPosition - startPosition).normalized;
         }
     }
 
     private void Awake()
     {
-        startPosition = transform.position;
+        rectTransform = GetComponent<RectTransform>();
+
+        startPosition = rectTransform.position;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -27,13 +35,13 @@ public class PlayerStickInput : MonoBehaviour, IMoveDirection, IEndDragHandler, 
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        transform.position = startPosition;
+        rectTransform.anchoredPosition = startPosition;
     }
 
-    private void MoveStick(Vector2 position)
+    private void MoveStick(Vector2 screenPosition)
     {
-        Vector2 delta = position - startPosition;
+        Vector2 delta = screenPosition - startPosition;
         delta = Vector2.ClampMagnitude(delta, movementRange);
-        transform.position = startPosition + delta;
+        rectTransform.anchoredPosition = startPosition + delta;
     }
 }
